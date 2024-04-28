9d1aceccd79b233e90511d33e682d126d27a2257
henrilaats <henrilaats55@gmail.com>
2024-04-28 22:06:52 +0300

    Api gateway

--- recipe-service/src/main/java/com/maitsev/recipeservice/RecipeServiceApplication.java
+++ recipe-service/src/main/java/com/maitsev/recipeservice/RecipeServiceApplication.java
@@ -15,10 +15,13 @@ public class RecipeServiceApplication {
 
 	@Bean
 	// WebClient.Builder is mutable builder for creating a WebClient.
-	// WebClient is an interface representing the main entry point for performing web requests.
-	// It is a part of Spring Web Reactive module and will be replacing the classic RestTemplate.
-	// Unlike RestTemplate (synchronous), WebClient  is a reactive (asynchronous), non-blocking solution that works over the HTTP/1.1 protocol.
+	// WebClient is an interface representing the main entry point for performing
+	// web requests.
+	// It is a part of Spring Web Reactive module and will be replacing the classic
+	// RestTemplate.
+	// Unlike RestTemplate (synchronous), WebClient is a reactive (asynchronous),
+	// non-blocking solution that works over the HTTP/1.1 protocol.
 	public WebClient.Builder getWebClientBuilder() {
-		return  WebClient.builder();
+		return WebClient.builder();
 	}
 }

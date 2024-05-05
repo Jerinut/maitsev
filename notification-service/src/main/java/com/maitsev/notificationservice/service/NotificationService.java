7259e09a5096c09d454d506fccc4749f2566a8f2
henrilaats <henrilaats55@gmail.com>
2024-05-05 23:25:57 +0300

    issue with review type

--- notification-service/src/main/java/com/maitsev/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/maitsev/notificationservice/service/NotificationService.java
@@ -1,8 +1,10 @@
 package com.maitsev.notificationservice.service;
+
 import com.maitsev.notificationservice.dto.NotificationDto;
 import com.maitsev.notificationservice.model.Chat;
 import com.maitsev.notificationservice.model.Message;
 import com.maitsev.notificationservice.model.Notification;
+import com.maitsev.notificationservice.model.Review;
 import com.maitsev.notificationservice.repository.NotificationRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -32,35 +34,35 @@ public class NotificationService {
     notificationRepository.findAll().forEach(notifications::add);
     return notifications.stream().map(this::mapToNotificationDto).toList();
   }
+
   private NotificationDto mapToNotificationDto(Notification notification) {
     return NotificationDto.builder()
-            .id(notification.getId())
-            .message(notification.getMessage())
-            .status(notification.getStatus())
-            .userId(notification.getUserId())
-            .build();
+        .id(notification.getId())
+        .message(notification.getMessage())
+        .status(notification.getStatus())
+        .userId(notification.getUserId())
+        .build();
   }
 
   public void addNotififcation(NotificationDto notififcationDto) {
     Notification notification = Notification.builder()
-            .id(notififcationDto.getId())
-            .status(notififcationDto.getStatus())
-            .message(notififcationDto.getMessage())
-            .userId(notififcationDto.getUserId())
-            .build();
+        .id(notififcationDto.getId())
+        .status(notififcationDto.getStatus())
+        .message(notififcationDto.getMessage())
+        .userId(notififcationDto.getUserId())
+        .build();
     notificationRepository.save(notification);
   }
 
-  public Optional <NotificationDto> getNotification(String id) {
-    Optional <Notification> notification = notificationRepository.findById(id);
+  public Optional<NotificationDto> getNotification(String id) {
+    Optional<Notification> notification = notificationRepository.findById(id);
     return notification.map(this::mapToNotificationDto);
   }
 
   public void deleteNotification(String id) {
     // Retrieve the chat from the database
     Notification notification = notificationRepository.findById(id)
-            .orElseThrow(() -> new EntityNotFoundException("notification not found with id " + id));
-
+        .orElseThrow(() -> new EntityNotFoundException("notification not found with id " + id));
 
     notificationRepository.delete(notification);
 
@@ -69,62 +71,80 @@ public class NotificationService {
 
   public void updateNotification(String id, NotificationDto notificationDto) {
     Notification notification = Notification.builder()
-            .id(notificationDto.getId())
-            .status(notificationDto.getStatus())
-            .userId(notificationDto.getUserId())
-            .message(notificationDto.getMessage())
-            .build();
+        .id(notificationDto.getId())
+        .status(notificationDto.getStatus())
+        .userId(notificationDto.getUserId())
+        .message(notificationDto.getMessage())
+        .build();
     notificationRepository.save(notification);
     log.info("Notification is updated", notification.getId());
   }
 
-  @KafkaListener(topics = "chatTopicJson", groupId = "chatEventGroupForChat" )
-  public void consumes(Chat chat){
+  @KafkaListener(topics = "chatTopicJson", groupId = "chatEventGroupForChat")
+  public void consumes(Chat chat) {
     log.info("Log message recieved from chat topic: {} ", chat.toString());
     String user1Id = chat.getUser1Id();
     String user2Id = chat.getUser2Id();
 
     // Create notifications for both users
     Notification notificationUser1 = Notification.builder()
-            .id(UUID.randomUUID().toString())
-            .status("NEW CHAT")
-            .userId(user1Id)
-            .message("You have a new chat. Yuppi!")
-            .build();
+        .id(UUID.randomUUID().toString())
+        .status("NEW CHAT")
+        .userId(user1Id)
+        .message("You have a new chat. Yuppi!")
+        .build();
 
     Notification notificationUser2 = Notification.builder()
-            .id(UUID.randomUUID().toString())
-            .status("NEW CHAT")
-            .userId(user2Id)
-            .message("You have a new chat. Yuppi!")
-            .build();
+        .id(UUID.randomUUID().toString())
+        .status("NEW CHAT")
+        .userId(user2Id)
+        .message("You have a new chat. Yuppi!")
+        .build();
 
     // Save notifications to the repository
     notificationRepository.save(notificationUser1);
     notificationRepository.save(notificationUser2);
   }
 
-  @KafkaListener(topics = "messageTopicJson", groupId = "chatEventGroupForChat" )
-  public void consumes(Message message){
+  @KafkaListener(topics = "messageTopicJson", groupId = "chatEventGroupForChat")
+  public void consumes(Message message) {
     log.info("Log message recieved from message topic: {} ", message.toString());
     String receiverId = message.getReceiver();
 
     // Create notifications for receiver
     Notification notificationUser1 = Notification.builder()
-            .id(UUID.randomUUID().toString())
-            .status("NEW MESSAGE")
-            .userId(receiverId)
-            .message("just new message")
-            .build();
-
+        .id(UUID.randomUUID().toString())
+        .status("NEW MESSAGE")
+        .userId(receiverId)
+        .message("just new message")
+        .build();
 
     // Save notification to the repository
     notificationRepository.save(notificationUser1);
   }
+
+  @KafkaListener(topics = "reviewTopicJson", groupId = "reviewEventGroup")
+  public void consumes(Review review) {
+    log.info("Log message recieved from review topic: {} ", review.toString());
+    // String receiverId = message.getReceiver();
+
+    // Create notifications for receiver
+    /*
+     * Notification notificationUser1 = Notification.builder()
+     * .id(UUID.randomUUID().toString())
+     * .status("NEW MESSAGE")
+     * .userId(receiverId)
+     * .message("just new message")
+     * .build();
+     */
+    // Save notification to the repository
+    // notificationRepository.save(notificationUser1);
+  }
+
   public List<NotificationDto> getNotificationsForUser(String userId) {
     List<Notification> notifications = notificationRepository.findByUserId(userId);
     return notifications.stream()
-            .map(this::mapToNotificationDto)
-            .collect(Collectors.toList());
+        .map(this::mapToNotificationDto)
+        .collect(Collectors.toList());
   }
 }

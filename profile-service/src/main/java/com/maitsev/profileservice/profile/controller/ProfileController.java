32aebcb860e312f6a3a4b07e8080328532ac97eb
jerin <jerin.george@ut.ee>
2024-05-05 12:45:29 +0300

    added notification service

--- profile-service/src/main/java/com/maitsev/profileservice/profile/controller/ProfileController.java
+++ profile-service/src/main/java/com/maitsev/profileservice/profile/controller/ProfileController.java
@@ -3,23 +3,17 @@ package com.maitsev.profileservice.profile.controller;
 import java.util.List;
 import java.util.Optional;
 
-import com.maitsev.postservice.post.dto.PostDto;
-import com.maitsev.profileservice.profile.model.Profile;
-import com.maitsev.profileservice.profile.service.KafkaProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.maitsev.profileservice.profile.dto.ProfileDto;
 import com.maitsev.profileservice.profile.service.ProfileService;
-import com.maitsev.recipeservice.recipe.dto.RecipeDto;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,8 +25,6 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
-    @Autowired
-    private KafkaProducerService kafkaService;
     @GetMapping("/profiles")
     public List<ProfileDto> getAllProfiles() {
         return profileService.getAllProfiles();

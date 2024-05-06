324479df0707cb696bf6b9e80ee7b5131bd0cce6
jerin <jerin.george@ut.ee>
2024-05-06 16:12:20 +0300

    api gateway added

--- profile-service/src/main/java/com/maitsev/profileservice/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/maitsev/profileservice/ProfileServiceApplication.java
@@ -2,9 +2,11 @@ package com.maitsev.profileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProfileServiceApplication {
 
